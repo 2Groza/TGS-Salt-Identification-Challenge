eafbd884c2b75873053eca608364bfd464f4c562
2Groza <42021504+2Groza@users.noreply.github.com>
2018-08-06 15:57:56 +0800

    Transformation Function
    This function can transform the final result as the challenge required in submission.

---
+++ 96-96test/to_final_result.py
@@ -0,0 +1,20 @@
+def to_final_result(line):
+    i = 1
+    starts = []
+    lenths = []
+    while i<=len(line):
+        j = 0
+        if line[i-1] == 1:
+            starts.append(i)
+            j = 1
+            while line[i-1+j]==1:
+                j+=1
+                if i-1+j>= len(line):
+                    break
+            lenths.append(j)
+        i += j+1
+    comma_split = []
+    for i in range(len(starts)):
+        comma_split.append(starts.pop(0))
+        comma_split.append(lenths.pop(0))
+    return comma_split
